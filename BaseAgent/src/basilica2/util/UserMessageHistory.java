5895e582bb36bded7eab8ce713f6255232625fcd
Amel <hamidovic@student.tugraz.at>
2020-10-26 12:38:09 +0100

    Added first try of rebo feedback

--- BaseAgent/src/basilica2/util/UserMessageHistory.java
+++ BaseAgent/src/basilica2/util/UserMessageHistory.java
@@ -8,18 +8,22 @@ import ratte.texttools;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class UserMessageHistory {
 
     private final int SHORT_MESSAGE_THRESHOLD = 4;
-    private final String remind_user_to_write_whole_sentences = "Heute wieder einmal kurz angebunden.";
-    private final String remind_user_to_write_to_be_precise = "Wie genau meinst du das?";
-    private ArrayList<MessageEvent> messages = new ArrayList<MessageEvent>();
-    private ArrayList<MessageEvent> multi_message_response_array = new ArrayList<MessageEvent>();
+    private static final String REMIND_USER_MESSAGE = "Heute wieder einmal kurz angebunden.";
+    private static final String FEEDBACK_PHRASE = "Das Gespräch war aus meiner Sicht";
+
+    private ArrayList<MessageEvent> messages = new ArrayList<>();
+    private ArrayList<MessageEvent> multiMessageResponseArray = new ArrayList<>();
+
     private boolean userPoked = false;
 
     public boolean multiMessageActive() {
-        return !multi_message_response_array.isEmpty() ? true : false;
+        return !multiMessageResponseArray.isEmpty();
     }
 
     private boolean checkMultiMessage(String text) {
@@ -39,17 +43,17 @@ public class UserMessageHistory {
     private void handleMultiLineMessage(MessageEvent event) {
         String message = event.getText().trim();
         if (checkMultiMessage(message)) {
-            multi_message_response_array.add(event);
-        } else if (!multi_message_response_array.isEmpty()) {
+            multiMessageResponseArray.add(event);
+        } else if (!multiMessageResponseArray.isEmpty()) {
 
-            Iterator<MessageEvent> iter = multi_message_response_array.iterator();
+            Iterator<MessageEvent> iter = multiMessageResponseArray.iterator();
             StringBuilder concatenatedMessage = new StringBuilder(iter.next().getText());
             while (iter.hasNext()) {
                 concatenatedMessage.append(" | ").append(iter.next().getText());
             }
             String text = concatenatedMessage.append(" | ").append(message).toString();
             event.setText(text);
-            multi_message_response_array.clear();
+            multiMessageResponseArray.clear();
         }
 
     }
@@ -74,16 +78,45 @@ public class UserMessageHistory {
 
     public void analyizeMessages(List<String> tutorTurns) {
         if (hasTooShortAnswers()) {
-            tutorTurns.add(0, remind_user_to_write_whole_sentences);
+            tutorTurns.add(0, REMIND_USER_MESSAGE);
         }
-        /*if (tutorTurns.contains())) {
-            //tutorTurns.add(0, remind_user_to_write_to_be_precise);
-        }
-        texttools tool = new texttools();
-        for (MessageEvent msg : messages) {
-            tool.set_text(msg.getText());
 
-        }*/
+        Optional<String> feedBackTurn = tutorTurns
+                .stream()
+                .filter(turn -> StringUtils.equals(turn, FEEDBACK_PHRASE)).findFirst();
+
+        if (feedBackTurn.isPresent()) {
+
+            String tutorAnswer = getTutorFeedBack();
+
+            tutorTurns.remove(0);
+            tutorTurns.add(0, FEEDBACK_PHRASE + tutorAnswer);
+        }
+    }
+
+    private String getTutorFeedBack() {
+
+        texttools ratteTool = new texttools();
+
+        String allAnswers = messages.stream()
+                .map(MessageEvent::getText)
+                .collect(Collectors.joining("; "));
+
+        ratteTool.set_text(allAnswers);
+        double rix = ratteTool.rix() * 100.0;
+        double wordCount = ratteTool.dwords();
+        int monosyllables = ratteTool.count_wordswithsyllabes_exact(1);
+        int trisyllables = ratteTool.count_wordswithsyllabes_exact(3);
+
+        double score = rix + wordCount + monosyllables + trisyllables;
+
+        if (score <= 200.0) {
+            return  " jetzt nicht so berauschend, du könntest dir schon etwas mehr Mühe geben.";
+        } else if (score >= 300.0) {
+            return " sehr cool, freut mich dass du dir so viel Mühe gibst :D";
+        } else {
+            return " ganz in Ordnung :)";
+        }
     }
 
     private int countWordsofString(String input) {
